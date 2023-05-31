b0c6d929420fe177caf573c85637882976d4aa8f
Rizwan Fahim <119812012+Gitrizwan-web@users.noreply.github.com>
2023-05-31 20:52:42 +0500

    pak-wheel

---
+++ script.js
@@ -0,0 +1,348 @@
+let products = {
+    data: [{
+            productName: "Toyota Corolla",
+            category: "Toyota",
+            price: "USD $22,945",
+            image: "Corolla-X-Cars-Cropped-Pictures-for-Website.jpg",
+        },
+        {
+            productName: "Toyota Yaris",
+            category: "Toyota",
+            price: " USD $82,945",
+            image: "1.jpg",
+        },
+        {
+            productName: "Toyota_Hilux-Single-Cabin",
+            category: "Toyota",
+            price: " USD $68,945",
+            image: "Toyota_Hilux-Single-Cabin.png",
+        },
+        {
+            productName: "Toyota Fortuner",
+            category: "Toyota",
+            price: " USD $62,945",
+            image: "Toyota_Fortuner.png",
+        },
+        {
+            productName: "Toyota_Land-Cruiser",
+            category: "Toyota",
+            price: " USD $62,945",
+            image: "Toyota_Land-Cruiser.webp",
+        },
+        {
+            productName: "Honda_Pilot",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Pilot.png ",
+        },
+        {
+            productName: "Hundayi_The-New-Sonata",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_The-New-Sonata.png ",
+        },
+        {
+            productName: "Toyota_Corolla-Cross-Rocco",
+            category: "Toyota",
+            price: " USD $59,945",
+            image: "Toyota_Corolla-Cross.png",
+        },
+        {
+            productName: "Toyota_Hilux",
+            category: "Toyota",
+            price: " USD $59,945",
+            image: "Toyota_Hilux.png",
+        },
+        {
+            productName: "Honda_Civic-Hatchback",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Civic-Hatchback.png",
+        },
+        {
+            productName: "Honda_Civic-Sedan",
+            category: "Honda",
+            price: "USD $89,945",
+            image: "Honda_Civic-Sedan.png",
+        },
+        {
+            productName: "Honda_CR-V-Hybrid",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_CR-V-Hybrid (1).png",
+        },
+        {
+            productName: "Honda_Civic-Accord-Sedan",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Civic-Accord-Sedan.avif",
+        },
+        {
+            productName: "Honda_HR-V",
+            category: "Honda",
+            price: "USD $70,945",
+            image: "Honda_HR-V.avif",
+        },
+        {
+            productName: "Honda_Passport",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Passport.avif",
+        },
+        {
+            productName: "Honda_Ridgeline",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Ridgeline.avif",
+        },
+        {
+            productName: "Hundayi_Veloster",
+            category: "Hundayi",
+            price: "USD $55,945",
+            image: "Hundayi_Veloster.png ",
+        },
+        {
+            productName: "Hundayi_Ioniq6",
+            category: "Hundayi",
+            price: "USD $79,945",
+            image: "Hundayi_Ioniq6.png ",
+        },
+        {
+            productName: "Hundayi_I30-Hybrid",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_I30-Hybrid.png ",
+        },
+        {
+            productName: "Hundayi_Elantra",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_Elantra.png ",
+        },
+        {
+            productName: "Hundayi_Elantra",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_Elantra.png ",
+        },
+        {
+            productName: "Hundayi_elantra-hybrid",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_elantra-hybrid.png ",
+        },
+        {
+            productName: "Toyota Corolla",
+            category: "Toyota",
+            price: "USD $22,945",
+            image: "Corolla-X-Cars-Cropped-Pictures-for-Website.jpg",
+        },
+        {
+            productName: "Toyota Yaris",
+            category: "Toyota",
+            price: " USD $82,945",
+            image: "1.jpg",
+        },
+        {
+            productName: "Toyota_Hilux-Single-Cabin",
+            category: "Toyota",
+            price: " USD $68,945",
+            image: "Toyota_Hilux-Single-Cabin.png",
+        },
+        {
+            productName: "Toyota Fortuner",
+            category: "Toyota",
+            price: " USD $62,945",
+            image: "Toyota_Fortuner.png",
+        },
+        {
+            productName: "Toyota_Land-Cruiser",
+            category: "Toyota",
+            price: " USD $62,945",
+            image: "Toyota_Land-Cruiser.webp",
+        },
+        {
+            productName: "Honda_Pilot",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Pilot.png ",
+        },
+        {
+            productName: "Hundayi_The-New-Sonata",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_The-New-Sonata.png ",
+        },
+        {
+            productName: "Toyota_Corolla-Cross-Rocco",
+            category: "Toyota",
+            price: " USD $59,945",
+            image: "Toyota_Corolla-Cross.png",
+        },
+        {
+            productName: "Toyota_Hilux",
+            category: "Toyota",
+            price: " USD $59,945",
+            image: "Toyota_Hilux.png",
+        },
+        {
+            productName: "Honda_Civic-Hatchback",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Civic-Hatchback.png",
+        },
+        {
+            productName: "Honda_Civic-Sedan",
+            category: "Honda",
+            price: "USD $89,945",
+            image: "Honda_Civic-Sedan.png",
+        },
+        {
+            productName: "Honda_CR-V-Hybrid",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_CR-V-Hybrid (1).png",
+        },
+        {
+            productName: "Honda_Civic-Accord-Sedan",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Civic-Accord-Sedan.avif",
+        },
+        {
+            productName: "Honda_HR-V",
+            category: "Honda",
+            price: "USD $70,945",
+            image: "Honda_HR-V.avif",
+        },
+        {
+            productName: "Honda_Passport",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Passport.avif",
+        },
+        {
+            productName: "Honda_Ridgeline",
+            category: "Honda",
+            price: "USD $69,945",
+            image: "Honda_Ridgeline.avif",
+        },
+        {
+            productName: "Hundayi_Veloster",
+            category: "Hundayi",
+            price: "USD $55,945",
+            image: "Hundayi_Veloster.png ",
+        },
+        {
+            productName: "Hundayi_Ioniq6",
+            category: "Hundayi",
+            price: "USD $79,945",
+            image: "Hundayi_Ioniq6.png ",
+        },
+        {
+            productName: "Hundayi_I30-Hybrid",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_I30-Hybrid.png ",
+        },
+        {
+            productName: "Hundayi_Elantra",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_Elantra.png ",
+        },
+        {
+            productName: "Hundayi_Elantra",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_Elantra.png ",
+        },
+        {
+            productName: "Hundayi_elantra-hybrid",
+            category: "Hundayi",
+            price: "USD $89,945",
+            image: "Hundayi_elantra-hybrid.png ",
+        },
+
+    ],
+};
+
+for (let i of products.data) {
+    let card = document.createElement("div");
+    card.classList.add("card", i.category, "hide");
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    let container = document.createElement("div");
+    container.classList.add("container");
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    container.appendChild(price)
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+}
+
+//parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        //check if value equals innerText
+        if (value.toUpperCase() == button.innerText.toUpperCase()) {
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }
+    });
+
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+        //display all cards on 'all' button click
+        if (value == "all") {
+            element.classList.remove("hide");
+        } else {
+            //Check if element contains category class
+            if (element.classList.contains(value)) {
+                //display element based on category
+                element.classList.remove("hide");
+            } else {
+                //hide other elements
+                element.classList.add("hide");
+            }
+        }
+    });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+
+    //loop through all elements
+    elements.forEach((element, index) => {
+        //check if text includes the search value
+        if (element.innerText.includes(searchInput.toUpperCase())) {
+            //display matching card
+            cards[index].classList.remove("hide");
+        } else {
+            //hide others
+            cards[index].classList.add("hide");
+        }
+    });
+});
+
+//Initially display all products
+window.onload = () => {
+    filterProduct("all");
+};
